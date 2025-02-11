5e3a178efd33a7292a7b6aaea6b74403cc6acf18
Kevin Crowe <339920+kcrowebasspro@users.noreply.github.com>
2025-02-10 22:12:06 -0400

    Activity 5

--- js/main.js
+++ js/main.js
@@ -1,1 +1,70 @@
-// Add all scripts to the JS folder
+//Set the mapbox key
+var accessToken = 'pk.eyJ1Ijoia2Nyb3dlYmFzc3BybyIsImEiOiJjbG8wZnJwMXgxNW1lMnNwZGF4M295bzhiIn0.HH0mKHddBIow2AdY707JMA';
+
+//declare map var in global scope
+var map;
+
+//function to instantiate the Leaflet map
+function createMap(){
+    //create the map
+    map = L.map('map', {
+        center: [41, -100],
+        zoom: 4
+    });
+
+
+    // Tiles are 512x512 pixels and are offset by 1 zoom level
+    L.tileLayer(
+        'https://api.mapbox.com/styles/v1/mapbox/light-v11/tiles/{z}/{x}/{y}?access_token=' + accessToken, {
+            tileSize: 512,
+            zoomOffset: -1,
+            attribution: '© <a href="https://www.mapbox.com/contribute/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        }).addTo(map);
+
+
+    //call getData function
+    getData(map);
+};
+
+//function to attach popups to each mapped feature
+function onEachFeature(feature, layer) {
+    //no property named popupContent; instead, create html string with all properties
+    var popupContent = "";
+    if (feature.properties) {
+        //loop to add feature property names and values to html string
+        for (var property in feature.properties){
+            popupContent += "<p>" + property + ": " + feature.properties[property] + "</p>";
+        }
+        layer.bindPopup(popupContent);
+    };
+};
+
+//function to retrieve the data and place it on the map
+function getData(){
+    //load the data
+    fetch("data/rents.geojson")
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(json){
+            //create marker options
+            var geojsonMarkerOptions = {
+                radius: 8,
+                fillColor: "#ff7800",
+                color: "#000",
+                weight: 1,
+                opacity: 1,
+                fillOpacity: 0.8
+            };
+            //create a Leaflet GeoJSON layer and add it to the map
+            L.geoJson(json, {
+                onEachFeature: onEachFeature,
+                pointToLayer: function (feature, latlng){
+                    return L.circleMarker(latlng, geojsonMarkerOptions);
+                }
+            }).addTo(map);
+        })
+        
+};
+
+document.addEventListener('DOMContentLoaded',createMap)// Add all scripts to the JS folder
